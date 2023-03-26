6325ebccd6561499324c347c26f051dd3510b9a7
Dare Victor Adebayo <43048158+vheckthor@users.noreply.github.com>
2023-03-26 23:17:37 +0100

    core setup

---
+++ app/core/security.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta
+from typing import Any, Union
+
+from jose import jwt
+from passlib.context import CryptContext
+
+from app.core import config
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+ALGORITHM = "HS256"
+
+
+def create_access_token(
+    subject: Union[str, Any], expires_delta: timedelta = None
+) -> str:
+    """
+    create_access_token
+
+    Args:
+        subject (Union[str, Any])
+        expires_delta (timedelta, optional) Defaults to None.
+
+    Returns:
+        str
+    """
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(
+            minutes=config.Settings().ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, config.Settings().SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    verify_password 
+
+    Args:
+        plain_password (str)
+        hashed_password (str)
+
+    Returns:
+        bool
+    """
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    """
+    get_password_hash 
+
+    Args:
+        password (str)
+
+    Returns:
+        str
+    """
+    return pwd_context.hash(password)
