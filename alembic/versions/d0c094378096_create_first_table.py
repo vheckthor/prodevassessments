117de9467eb65715c2f2eaf3d62611be58b0ef56
Dare Victor Adebayo <43048158+vheckthor@users.noreply.github.com>
2023-03-26 23:18:51 +0100

    first and second migrations

---
+++ alembic/versions/d0c094378096_create_first_table.py
@@ -0,0 +1,24 @@
+"""create first table
+
+Revision ID: d0c094378096
+Revises: 
+Create Date: 2023-03-26 22:37:01.487669
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd0c094378096'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
